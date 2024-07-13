b77129cc6658519d0e862829321e7e03ebfd680f
Marvellous Divine <marvydee1@gmail.com>
2024-07-13 23:37:22 +0100

    Update login.js

--- frontend/assets/js/login.js
+++ frontend/assets/js/login.js
@@ -4,7 +4,7 @@ document.getElementById("login-form").addEventListener("submit", async (e) => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const response = await fetch("https://qfledge.vercel.app/login", {
+  const response = await fetch("https://qfledge-1.onrender.com/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password }),
