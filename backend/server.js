59328dc393ae5f0f59e1121568a7ab7d475f92ac
Marvellous Divine <marvydee1@gmail.com>
2024-07-13 07:44:57 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 // Middleware
 app.use(
   cors({
-    origin: "https://qfsdappsledger.com/", 
+    origin: "*", 
     methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
