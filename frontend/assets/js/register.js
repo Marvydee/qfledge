7cfceb6366d1d85fe188320215eb55f2d66341a1
Marvellous Divine <marvydee1@gmail.com>
2024-07-13 23:38:33 +0100

    Update register.js

--- frontend/assets/js/register.js
+++ frontend/assets/js/register.js
@@ -7,7 +7,7 @@ document
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const response = await fetch("https://qfledge.vercel.app/register", {
+    const response = await fetch("https://qfledge-1.onrender.com/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, email, password }),
