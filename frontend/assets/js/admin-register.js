bff94913ae0d44f2aca89b199a0c4a547a23f1f6
Marvellous Divine <marvydee1@gmail.com>
2024-07-24 23:47:12 +0100

    Update admin-register.js

--- frontend/assets/js/admin-register.js
+++ frontend/assets/js/admin-register.js
@@ -8,13 +8,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const password = form.elements.password.value;
 
     try {
-      const response = await fetch("http://localhost:3000/admin/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, email, password }),
-      });
+      const response = await fetch(
+        "https://qfledge-1.onrender.com/admin/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, email, password }),
+        }
+      );
 
       if (!response.ok) {
         throw new Error("Failed to register admin");
