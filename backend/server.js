e8363665fca7011cfa81824e03b8c735672ef655
marvellousd <marvellousd46@gmail.com>
2024-07-30 18:04:25 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -101,7 +101,7 @@ app.get("/user/check-admin", isAuthorized, (req, res) => {
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, path.join(__dirname, 'public/uploads'));
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname)); // Unique filename
